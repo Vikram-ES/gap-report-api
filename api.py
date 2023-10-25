352f828c92f2d22787c04203f71d87b60acf9e8b
Vikram-ES <vikram.k@exactspace.co>
2023-10-25 17:45:36 +0530

    adding subsciber

--- api.py
+++ api.py
@@ -1,9 +1,29 @@
 from flask import Flask, jsonify
+import paho.mqtt.client as mqtt
 
-# Create a Flask application
-app = Flask(__name__)
+app = Flask(__name)
 
-# Define a simple endpoint
+# Define the MQTT broker (replace with your MQTT broker address)
+broker_address = "mqtt.eclipse.org"
+
+# Create an MQTT client for subscription
+mqtt_client = mqtt.Client("subscriber")
+
+# Define a callback function to handle incoming messages
+def on_message(client, userdata, message):
+    payload = message.payload.decode()
+    print(f"Received message: {payload}")
+
+# Set the callback function for the MQTT client
+mqtt_client.on_message = on_message
+
+# Connect to the MQTT broker
+mqtt_client.connect(broker_address)
+# Subscribe to the topic you want to listen to
+mqtt_client.subscribe("example_topic")
+# Start the MQTT client loop in the background
+mqtt_client.loop_start()
+
 @app.route('/')
 def hello_world():
     return jsonify(message="Hello, World!")
