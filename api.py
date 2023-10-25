ddc0d6647934c8556dd8d0825e2c6c9a4b18dcc8
Vikram-ES <vikram.k@exactspace.co>
2023-10-25 18:05:22 +0530

    pub sub working fine

--- api.py
+++ api.py
@@ -1,18 +1,21 @@
 from flask import Flask, jsonify
 import paho.mqtt.client as mqtt
 
-app = Flask(__name)
+app = Flask(__name__)
 
-# Define the MQTT broker (replace with your MQTT broker address)
-broker_address = "mqtt.eclipse.org"
+# Define the MQTT broker (use localhost for testing)
+broker_address = "localhost"
 
 # Create an MQTT client for subscription
 mqtt_client = mqtt.Client("subscriber")
 
 # Define a callback function to handle incoming messages
 def on_message(client, userdata, message):
-    payload = message.payload.decode()
-    print(f"Received message: {payload}")
+    if client.is_connected():
+        payload = message.payload.decode()
+        print(f"Received message: {payload}")  # Print the received message
+    else:
+        print("Not connected to the MQTT broker. Message not processed.")
 
 # Set the callback function for the MQTT client
 mqtt_client.on_message = on_message
@@ -20,7 +23,8 @@ mqtt_client.on_message = on_message
 # Connect to the MQTT broker
 mqtt_client.connect(broker_address)
 # Subscribe to the topic you want to listen to
-mqtt_client.subscribe("example_topic")
+topic = "example_topic"  # Make sure this topic matches the one used by the publisher
+mqtt_client.subscribe(topic)
 # Start the MQTT client loop in the background
 mqtt_client.loop_start()
 
@@ -29,4 +33,4 @@ def hello_world():
     return jsonify(message="Hello, World!")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- pub.py
+++ pub.py
@@ -1,7 +1,8 @@
 import paho.mqtt.client as mqtt
+import time
 
-# Define the MQTT broker (replace with your MQTT broker address)
-broker_address = "mqtt.eclipse.org"
+# Define the MQTT broker (use localhost for testing)
+broker_address = "localhost"
 
 # Create an MQTT client
 client = mqtt.Client("publisher")
@@ -9,12 +10,16 @@ client = mqtt.Client("publisher")
 # Connect to the broker
 client.connect(broker_address)
 
-# Publish a message to a topic
+# Publish a message to a topic every 10 seconds
 topic = "example_topic"
-message = "Hello, MQTT!"
+response_number = 1  # Initialize the response number
 
-# Publish the message
-client.publish(topic, message)
+while True:
+    message = f"Response {response_number}: Hello, MQTT!"
+    client.publish(topic, message)
+    print(f"Published message: {message}")
+    response_number += 1  # Increment the response number
+    time.sleep(10)  # Wait for 10 seconds before publishing the next message
 
-# Disconnect from the broker
+# Disconnect from the broker (This will never be reached in this code)
 client.disconnect()
